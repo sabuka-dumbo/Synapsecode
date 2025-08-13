4c18086c6df730200909e4e3f67ba47eb97cefbe
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-13 21:17:49 +0000

    Aug 14, 2025, 1:17 AM

--- synapsecode101/views.py
+++ synapsecode101/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
