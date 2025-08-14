ed2443a5be008ab1541d1f739f8891e613d63a11
Sabuka-Dumbo <anonym.sas@proton.me>
2025-08-14 11:18:03 +0000

    Aug 14, 2025, 3:18 PM

--- synapsecode101/models.py
+++ synapsecode101/models.py
@@ -5,3 +5,22 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Courses(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    instructor = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    homeworks_count = models.IntegerField(default=1)
+    lecture_count = models.IntegerField(default=1)
+    laboratory_count = models.IntegerField(default=0)
+    final_project_count = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+    
+class Lecture(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    
